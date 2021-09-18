5811a14af7bcb2280f3001f125bd2c37e8e5a469
angelchica <78941682+angelchica@users.noreply.github.com>
2021-09-17 19:52:06 -0500

    taller js

---
+++ ejem5.js
@@ -0,0 +1,4 @@
+const player = 'Skye';
+player = 'Chase';
+console.log(player);
+//Error de tipo no detectado: asignación a una variable constante
---
+++ ejem6.js
@@ -0,0 +1,11 @@
+//BLOQUE A
+var numeroParaAnalizar = 5;
+var numeroParaAnalizar = 20;
+console.log(numeroParaAnalizar);
+/*
+BLOQUE B
+let numeroParaAnalizar = 5;
+let numeroParaAnalizar = 20;
+console.log(numeroParaAnalizar);*/
+
+//Error de sintaxis no capturada: el identificador 'numeroParaAnalizar' ya ha sido declarado
